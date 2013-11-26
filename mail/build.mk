cc803c66b601dcca766937b5eebfa5173943f608
Joshua Cranmer <Pidgeot18@gmail.com>
2013-11-25 18:58:59 -0600

    Bug 648979, part 5: Fix packaging for the new configuration, r=Standard8

--- mail/build.mk
+++ mail/build.mk
@@ -2,12 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-ifdef COMM_BUILD
-ifndef MOZ_INCOMPLETE_EXTERNAL_LINKAGE
-# workaround Bug 599809 by making these makefiles be generated here
-SUBMAKEFILES += $(addsuffix /Makefile, $(APP_LIBXUL_DIRS) $(APP_LIBXUL_STATICDIRS))
-endif
-
 installer:
 	@$(MAKE) -C mail/installer installer
 
@@ -45,5 +39,3 @@ l10n-check::
 ifdef ENABLE_TESTS
 include $(srcdir)/mail/testsuite-targets.mk
 endif
-
-endif # COMM_BUILD
